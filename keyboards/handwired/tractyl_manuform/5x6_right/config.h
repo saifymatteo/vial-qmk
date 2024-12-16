07c48641042bf3aad61d4ffbf7d3d9fff3cd2d26
Saiful Mashuri <dev@saifulmashuri.com>
2024-12-16 23:35:50 +0800

    [keyboards] fix (Tractyl Manuform): undo main config.h

--- keyboards/handwired/tractyl_manuform/5x6_right/config.h
+++ keyboards/handwired/tractyl_manuform/5x6_right/config.h
@@ -21,21 +21,12 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define ROTATIONAL_TRANSFORM_ANGLE  -25
 #define POINTING_DEVICE_INVERT_X
 
-#define DYNAMIC_KEYMAP_LAYER_COUNT  5
+#define DYNAMIC_KEYMAP_LAYER_COUNT  16
 #define LAYER_STATE_16BIT
 
 /* disable action features */
 //#define NO_ACTION_LAYER
 //#define NO_ACTION_TAPPING
-#define NO_ACTION_ONESHOT
-#define DISABLE_LEADER
-
-/* disable debug for catalina space */
-#ifndef NO_DEBUG
-#define NO_DEBUG
-#endif // !NO_DEBUG
-#if !defined(NO_PRINT) && !defined(CONSOLE_ENABLE)
-#define NO_PRINT
-#endif // !NO_PRINT
+//#define NO_ACTION_ONESHOT
 
 #define POINTING_DEVICE_RIGHT
