d382489ae6fe2d7d4d5bdca5674288def4993841
Saiful Mashuri <dev@saifulmashuri.com>
2024-12-17 00:05:00 +0800

    [keyboards] fix (Tractyl Manuform): undo changes on main config.h

--- keyboards/handwired/tractyl_manuform/5x6_right/config.h
+++ keyboards/handwired/tractyl_manuform/5x6_right/config.h
@@ -24,6 +24,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define DYNAMIC_KEYMAP_LAYER_COUNT  16
 #define LAYER_STATE_16BIT
 
+
 /* disable action features */
 //#define NO_ACTION_LAYER
 //#define NO_ACTION_TAPPING
